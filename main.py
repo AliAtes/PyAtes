6c4f4a517215fa08c189c263e3051112cc7e520f
AliAtes <ali.ates@outlook.com>
2022-11-25 10:41:05 +0300

    .

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from core import LogisticRegression as logReg
 if __name__ == '__main__':
     # linReg.LinearRegression().simple_linear_regression()
     # linReg.LinearRegression().simple_array_linear_regression()
-    linReg.LinearRegression().multiple_3D_linear_regression()
+    # linReg.LinearRegression().multiple_3D_linear_regression()
     # linReg.LinearRegression().multiple_array_linear_regression()
-    # logReg.LogisticRegression().fit()
+    logReg.LogisticRegression().fit()
